c3f344680fa0b9f4d42bbd1b69510e714a3a9b42
maria-ishchuk <mariya.ishchuk@me.com>
2017-05-09 10:29:00 +0300

    gulp changed folder structure

---
+++ app/main.js
@@ -0,0 +1,100 @@
+// MASONRY
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+		var elem = document.querySelector('.grid');
+		var msnry = new Masonry( elem, {
+				// options
+				itemSelector: '.grid-item',
+				columnWidth: 263,
+				gutter: 30,
+				fitWidth: true
+		});
+
+
+		//title carousel functionality_____________________________________
+		var currentIndex = 0;
+		var carouselTimer, carouselInterval;
+
+		$('.jcarousel')
+			.jcarousel({wrap: "circular"})
+			.jcarouselAutoscroll({
+					interval: 3000,
+					target: '+=1',
+					autostart: true
+			});
+
+		$('#carousel').mouseover(function() {
+			$('.jcarousel').jcarouselAutoscroll("stop");
+		});
+
+		$('#carousel').mouseout(function() {
+			$('.jcarousel').jcarouselAutoscroll("start");
+		});
+
+		$('.next-carousel-button').click(function(){
+				$('.jcarousel').jcarousel('scroll', "+=1");
+		});
+
+		$('.prev-carousel-button').click(function(){
+				$('.jcarousel').jcarousel('scroll', "-=1");
+		});
+		//_____________________________________________________________________
+
+		// articles carousel___________________________________________________
+		$('.jcarousel-articles')
+			.jcarousel({wrap: "circular"})
+			.jcarouselAutoscroll({
+					interval: 3000,
+					target: '+=1',
+					autostart: true
+			});
+		 //____________________________________________________________________
+});
+
+// MAP
+function initMap() {
+	var kromleh = {lat: 48.197066, lng:35.177004};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 12,
+		center: kromleh,
+		disableDefaultUI: true,
+		scrollwheel:  false,
+	});
+};
+
+var openMap = document.getElementById('open-map');
+var parkMap = document.getElementById('parkmap');
+var captionLogo = document.getElementsByClassName('caption-logo3')[0];
+var parkmapP = document.getElementById('parkmap-p');
+var mapContainer = document.getElementsByClassName('map-container')[0];
+var grey = document.getElementsByClassName('grey')[0];
+var map = document.getElementById('map');
+var closeMap = document.getElementById('close-map');
+
+closeMap.style.display = 'none';
+
+openMap.onclick = function resizeMap() {
+		parkMap.style.height = '100vh';
+		parkMap.style.width = '100%';
+		captionLogo.style.display = 'none';
+		parkmapP.style.display = 'none';
+		mapContainer.style.height = '100%';
+		mapContainer.style.width = '100%';
+		grey.style.display = 'none';
+		openMap.style.display = 'none';
+		google.maps.event.trigger(map, "resize");
+		closeMap.style.display = 'block';
+};
+
+closeMap.onclick = function resizeMap() {
+		parkMap.style.height = '600px';
+		parkMap.style.width = '100%';
+		captionLogo.style.display = 'flex';
+		parkmapP.style.display = 'flex';
+		mapContainer.style.height = '234px';
+		mapContainer.style.width = '100%';
+		grey.style.display = 'block';
+		openMap.style.display = 'block';
+		google.maps.event.trigger(map, "resize");
+		closeMap.style.display = 'none';
+};
--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,8 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     autoprefixer = require('gulp-autoprefixer'),
     mainBowerFiles = require('main-bower-files'),
-    gulpSequence = require('gulp-sequence')
+    gulpSequence = require('gulp-sequence'),
+    rename = require('gulp-rename'),
     imagemin = require('gulp-imagemin');
 
 gulp.task('bowerfiles', function(cb) {
@@ -17,31 +18,96 @@ gulp.task('bowerfiles', function(cb) {
     gulp.src(mainBowerFiles()),
     // uglify(),
     concat('vendor.min.js'),
-    gulp.dest('dist/js/vendor')
+    gulp.dest('dist/js/lib')
   ],
-  cb)
+  cb);
 });
 
 gulp.task('js', function (cb) {
-   pump([
-        gulp.src('./assets/js/*.js'),
-        concat('app.js'),
-        // uglify(),
-        gulp.dest('dist/js/')
-    ],
-    cb
-  );
+  pump([
+    gulp.src([
+      './app/*.js',
+      './app/pages/**/*.js',
+      './app/shared/**/*.js']),
+    rename({dirname: ''}),
+    // concat('app.js'),
+    gulp.dest('dist/js')
+  ],
+  cb);
+});
+
+gulp.task('minify-js', function(cb) {
+  pump([
+    gulp.src('./dist/js/*.js'),
+    // concat('app.js'),
+    uglify(),
+    gulp.dest('dist/js/')
+  ],
+  cb);
 });
 
 gulp.task('html', function() {
-  return gulp.src('./*.html')
+  return gulp.src([
+      './app/index.html',
+      './app/pages/**/*.html'
+    ])
+    .pipe(rename({dirname: ''}))
     .pipe(gulp.dest('dist'));
 });
 
+gulp.task('shared-html', function() {
+  return gulp.src([
+      './app/shared/**/*.html'
+    ])
+    .pipe(rename({dirname: ''}))
+    .pipe(gulp.dest('dist/html'));
+});
+
 gulp.task('minify-html', function() {
-  return gulp.src('./dist/*.html')
+  return gulp.src([
+      './dist/*.html',
+      './dist/html/*.html'
+    ])
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('css', function() {
+  return gulp.src([
+      './app/assets/styles/*.css',
+      './app/pages/**/*.css',
+      './app/shared/**/*.css'
+    ])
+    // .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+    // .pipe(concat('style.min.css'))
+    .pipe(rename({dirname: ''}))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('./dist/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/css'));
+});
+
+// Fonts
+gulp.task('fonts', function() {
+  return gulp.src('./app/assets/fonts/*/*')
+    .pipe(gulp.dest('dist/fonts/'));
+});
+
+// Fonts
+gulp.task('image', function() {
+  return gulp.src('.app/assets/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('clean', function () {
+  return del([
+    'dist/*'
+  ]);
 });
 
 gulp.task('reload', function () {
@@ -49,32 +115,6 @@ gulp.task('reload', function () {
     .pipe(connect.reload());
 });
 
-gulp.task('css', function() {
-  return gulp.src([
-      './assets/css/reset.css',
-      './assets/css/carousel.css',
-      './assets/css/font-awesome.css',
-      './assets/css/nature-history.css',
-      './assets/css/main.css'])
-    // .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
-    .pipe(concat('style.min.css'))
-    .pipe(gulp.dest('dist/css'));
-});
-
-// Fonts
-gulp.task('fonts', function() {
-  return gulp.src([
-    './assets/fonts/*/*'])
-    .pipe(gulp.dest('dist/fonts'));
-});
-
-// Fonts
-gulp.task('img', function() {
-  return gulp.src(['./assets/img/*'])
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/img'));
-});
 
 gulp.task('connect', function() {
   connect.server({
@@ -83,7 +123,7 @@ gulp.task('connect', function() {
   });
 });
 
-gulp.task('app', function(){
+gulp.task('app', function() {
   var options = {
     uri: 'http://localhost:8080',
     app: 'chrome'
@@ -93,23 +133,26 @@ gulp.task('app', function(){
 });
 
 gulp.task('watch', function () {
-  return gulp.watch(['./*.html', './assets/js/main.js', './assets/css/*'], ['onWatchChange']);
-});
-
-gulp.task('clean', function () {
-  return del([
-    'dist/*'
-  ]);
+  return gulp.watch(
+    [
+      './app/*',
+      './app/pages/**/*',
+      './app/shared/**/*',
+      './app/assets/styles/*',
+    ],
+    ['onWatchChange']);
 });
 
 gulp.task('onWatchChange', function(cb) {
   gulpSequence(['html', 'js', 'css'], 'reload')(cb);
 });
 
-gulp.task('dist', gulpSequence('bowerfiles', 'js', ['html', 'css', 'fonts', 'img']));
+gulp.task('dist', gulpSequence('bowerfiles', 'js', ['html', 'shared-html', 'css', 'fonts', 'image']));
+
+gulp.task('minify', ['minify-js', 'minify-html', 'minify-css']);
 
 gulp.task('default', gulpSequence('clean', 'dist', 'connect', 'watch', 'app'));
 
-gulp.task('prod', gulpSequence('clean', 'dist'));
+gulp.task('prod', gulpSequence('clean', 'dist', 'minify'));
 
 
