17b5d20dc30a3d58da5bd71978555267e525f25c
Mariya Ishchuk <mariya.ishchuk@me.com>
2017-06-09 18:31:51 +0300

    General fixes

---
+++ app/pages/what-is-dp/what-is-dp.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+	var articles = [
+	]
+	var activeFilter = "nature";
+	var sidebar = document.getElementsByClassName("sidebar")[0];
+
+	sidebar.onclick = function(event) {
+		var target = event.target,
+			activeAccordion = document.querySelector(".accordion.active");
+
+		if(target.classList.contains("accordion")) {
+			activeFilter = target.dataset.filter;
+			setBreadcrumbs(target.dataset.filter);
+			activeAccordion && activeAccordion.classList.remove("active");
+			target.classList.toggle("active");
+			document.querySelector(".article.active") &&
+				document.querySelector(".article.active").classList.remove("active");
+			document.querySelector("[data-article='"+target.dataset.filter+"']") &&
+				document.querySelector("[data-article='"+target.dataset.filter+"']").classList.add("active");
+		}
+	}
+
+	function setBreadcrumbs(text) {
+		document.getElementsByClassName("dynamic-breadcrumb")[0].innerHTML = document.querySelector("[data-filter='"+text+"']").innerText;
+	};
+	setBreadcrumbs(activeFilter);
+});
