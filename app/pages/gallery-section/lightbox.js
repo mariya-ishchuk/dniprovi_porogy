6df45416557f74116608d07cb41f18a47f7577f5
vetalcore <vetalcore@gmail.com>
2017-06-19 22:40:27 +0300

    removed callbacks

--- app/pages/gallery-section/lightbox.js
+++ app/pages/gallery-section/lightbox.js
@@ -3,18 +3,6 @@ $(document).ready(function() {
 		type: 'image',
 		gallery: {
 			enabled: true
-		},
-		callbacks: {
-			resize: changeImgSize,
-			imageLoadComplete:changeImgSize,
-			change:changeImgSize
 		}
 	});
 });
-
-function changeImgSize(){
-	var img = this.content.find('img');
-	img.css('max-height', '100%');
-	img.css('width', 'auto');
-	img.css('max-width', 'auto');
-}
