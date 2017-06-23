583fb3af2be1d0007e72b9527a11f30a8b3779f9
Mariya Ishchuk <mariya.ishchuk@me.com>
2017-06-23 11:43:49 +0300

    Css optimization

--- gulp-tasks/css.js
+++ gulp-tasks/css.js
@@ -18,7 +18,7 @@ module.exports = function (gulp, plugins, options) {
     return plugins.mergeStream(gulp.src(options.addExtToPath(src, "css")), lessStream)
       // .pipe(plugins.if(options.isProd, plugins.cleanCss({compatibility: "ie8"})))
       .pipe(plugins.concat("main.css"))
-      .pipe(plugins.csslint())
+      // .pipe(plugins.csslint())
       .pipe(plugins.csslint.formatter())
       .pipe(plugins.autoprefixer("last 2 version", "safari 5", "ie 8", "ie 9"))
       // .pipe(plugins.rename({dirname: ""}))
